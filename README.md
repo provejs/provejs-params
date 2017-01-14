e18ac0d8a4c1a7ce02b2675f79df0031411a1089
Daniel Hollenbeck <dan@hollenbecks.com>
2017-01-14 11:21:41 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,142 @@
 # prove-params
+
+
+Prove function parameters using a simple schema.
+
+```js
+var Prove = require('prove-params');
+
+function myFunction(myArray, myNumber) {
+	Prove('ANF', arguments);
+}
+
+myFunction([], []);
+```
+will result in
+```bash
+{ Error: Argument #2: Expected function but got array
+    at myFunction (/Users/dan/repos/.../test.js:122:2)
+    at Object.<anonymous> (/Users/dan/repos/.../test.js:125:1)
+    at Module._compile (module.js:570:32)
+    at Object.Module._extensions..js (module.js:579:10)
+    at Module.load (module.js:487:32)
+    at tryModuleLoad (module.js:446:12)
+    at Function.Module._load (module.js:438:3)
+    at Module.runMain (module.js:604:10)
+    at run (bootstrap_node.js:394:7)
+    at startup (bootstrap_node.js:149:9)
+    at bootstrap_node.js:509:3 code: 'EINVALIDTYPE' }
+```
+
+Supported Types
+<table>
+<tr>
+<th>Letter</th>
+<th>Data Types</th>
+<th>Required</th>
+<tr>
+
+<tr>
+<td>*</td>
+<td>matches any type</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>A</td>
+<td>Array or Arguments</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>S</td>
+<td>String</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>N</td>
+<td>Number</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>F</td>
+<td>Function</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>O</td>
+<td>Object</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>B</td>
+<td>Boolean</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>G</td>
+<td>Arguments</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>E</td>
+<td>Error</td>
+<td>true</td>
+<tr>
+
+<tr>
+<td>A</td>
+<td>Array or Arguments</td>
+<td>false</td>
+<tr>
+
+<tr>
+<td>S</td>
+<td>String</td>
+<td>false</td>
+<tr>
+
+<tr>
+<td>N</td>
+<td>Number</td>
+<td>false</td>
+<tr>
+
+<tr>
+<td>F</td>
+<td>Function</td>
+<td>false</td>
+<tr>
+
+<tr>
+<td>O</td>
+<td>Object</td>
+<td>falsee</td>
+<tr>
+
+<tr>
+<td>B</td>
+<td>Boolean</td>
+<td>false</td>
+<tr>
+
+<tr>
+<td>G</td>
+<td>Arguments</td>
+<td>false</td>
+<tr>
+
+<tr>
+<td>E</td>
+<td>Error</td>
+<td>false</td>
+<tr>
+</table>
+
+This work was inspired by: https://github.com/iarna/aproba
---
+++ index.js
@@ -0,0 +1,136 @@
+'use strict';
+
+function isAny() {
+	return true;
+}
+
+function isMissing(arg) {
+	return arg == null;
+}
+
+function isArguments(arg) {
+	return isObject(arg) && arg.hasOwnProperty('callee');
+}
+
+function isArray(arg) {
+	//console.log('isArray()', arg);
+	var isArr = Array.isArray(arg);
+	var isArg = isObject(arg) && arg.hasOwnProperty('callee');
+	return isArr || isArg;
+}
+
+function isString(arg) {
+	return typeof arg === 'string';
+}
+
+function isNumber(arg) {
+	return typeof arg === 'number';
+}
+
+function isFunction(arg) {
+	return typeof arg === 'function';
+}
+
+function isObject(arg) {
+	var isObj = (typeof arg === 'object');
+	var isArr = Array.isArray(arg);
+	var isErr = arg instanceof Error;
+	return isObj && !isArr && !isErr;
+}
+
+function isBoolean(arg) {
+	return typeof arg === 'boolean';
+}
+
+function isError(arg) {
+	return arg instanceof Error;
+}
+
+var dsls = {
+	'*': ['any', isAny],
+
+	'A': ['array', isArray, true],
+	'S': ['string', isString, true],
+	'N': ['number', isNumber, true],
+	'F': ['function', isFunction, true],
+	'O': ['object', isObject, true],
+	'B': ['boolean', isBoolean, true],
+	'G': ['arguments', isArguments, true],
+	'E': ['error', isError, true],
+
+	'a': ['array', isArray, false],
+	's': ['string', isString, false],
+	'n': ['number', isNumber, false],
+	'f': ['function', isFunction, false],
+	'o': ['object', isObject, false],
+	'b': ['boolean', isBoolean, false],
+	'g': ['arguments', isArguments, true],
+	'e': ['error', isError, false]
+};
+
+var prove = function(schema, args) {
+
+	// validate
+	if (!schema) throw missing(0, 'schema');
+	if (!args) throw missing(1, 'args');
+	if (!isString(schema)) throw invalid(0, 'string', schema);
+	if (!isArguments(args) && !isArray(args)) throw invalid(0, 'arguments or array', args);
+
+	for (var i = 0; i < schema.length; ++i) {
+		proveOne(schema[i], args[i], i);
+	}
+
+	if (schema.length < args.length) throw tooMany(schema, args);
+};
+
+function proveOne(type, arg, i) {
+
+	var dsl = dsls[type], label, check, required;
+
+    if (!dsl) throw unsupported(i, type);
+    label = dsl[0];
+    check = dsl[1];
+	required = dsl[2];
+
+	if (!required && isMissing(arg)) return;
+    if (isMissing(arg)) throw missing(i, label);
+    if (!check(arg)) throw invalid(i, label, arg);
+}
+
+function whatType(arg) {
+	var what;
+	Object.keys(dsls).forEach(function (type) {
+		var dsl = dsls[type];
+		var check = dsl[1];
+		var label = dsl[0];
+		var is = check(arg);
+		if (is) what = label;
+	});
+	return what;
+}
+
+function unsupported(i, type) {
+	return newError('EUNKNOWNTYPE', 'Unknown type `' + type + '` in argument #' + (i + 1));
+}
+
+function invalid(i, expected, arg) {
+	var got = whatType(arg);
+    return newError('EINVALIDTYPE', 'Argument #' + (i + 1) + ': Expected `' + expected + '` but got `' + got + '`');
+}
+
+function missing(i, expected) {
+	return newError('EMISSINGARG', 'Missing required `' + expected + '` argument #' + (i + 1));
+}
+
+function tooMany(expected, got) {
+	return newError('ETOOMANYARGS', 'Too many arguments, expected ' + expected.length + ' and got ' + got.length);
+}
+
+function newError(code, msg) {
+	var e = new Error(msg);
+	e.code = code;
+	Error.captureStackTrace(e, prove);
+	return e;
+}
+
+module.exports = prove;
